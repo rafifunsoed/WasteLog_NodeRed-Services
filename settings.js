3d8cf46820f3c5d0131d6cd287fda27f79a5bda3
rafifunsoed <150503940+rafifunsoed@users.noreply.github.com>
2025-07-12 14:43:18 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,12 +6,12 @@ module.exports = {
             password: "$2a$12$GHbrRt1oMgmc1OfhIZfYjeHc6JwbeY92WScs./C1fYjw.vaRj4KeK",
             permissions: "*"
         }]
-    },
+    // },
 
-    // Mengaktifkan penyimpanan context ke file di disk
-    contextStorage: {
-        default: {
-            module: "localfilesystem"
-        }
-    }
+    // // Mengaktifkan penyimpanan context ke file di disk
+    // contextStorage: {
+    //     default: {
+    //         module: "localfilesystem"
+    //     }
+    // }
 };
