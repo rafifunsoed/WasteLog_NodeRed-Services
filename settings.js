ea701c51810b460cd1f899c47f44e9d9bc40d1f0
rafifunsoed <150503940+rafifunsoed@users.noreply.github.com>
2025-07-12 14:55:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,19 +1,16 @@
 module.exports = {
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "WasteLog",
+            password: "$2a$12$GHbrRt1oMgmc1OfhIZfYjeHc6JwbeY92WScs./C1fYjw.vaRj4KeK",
+            permissions: "*"
+        }]
+    },
 
-    adminAuth: {
-
-        type: "credentials",
-
-        users: [{
-
-            username: "WasteLog",
-
-            password: "$2a$12$GHbrRt1oMgmc1OfhIZfYjeHc6JwbeY92WScs./C1fYjw.vaRj4KeK",
-
-            permissions: "*"
-
-        }]
-
-    }
-
+    contextStorage: {
+        default: {
+            module: "localfilesystem"
+        }
+    }
 };
