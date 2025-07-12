a1dda75dc242918bd22e55e5cde39f64bab6cac8
rafifunsoed <150503940+rafifunsoed@users.noreply.github.com>
2025-07-12 14:46:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,17 +1,19 @@
 module.exports = {
-    adminAuth: {
-        type: "credentials",
-        users: [{
-            username: "WasteLog",
-            password: "$2a$12$GHbrRt1oMgmc1OfhIZfYjeHc6JwbeY92WScs./C1fYjw.vaRj4KeK",
-            permissions: "*"
-        }]
-    // },
 
-    // // Mengaktifkan penyimpanan context ke file di disk
-    // contextStorage: {
-    //     default: {
-    //         module: "localfilesystem"
-    //     }
-    // }
+    adminAuth: {
+
+        type: "credentials",
+
+        users: [{
+
+            username: "WasteLog",
+
+            password: "$2a$12$GHbrRt1oMgmc1OfhIZfYjeHc6JwbeY92WScs./C1fYjw.vaRj4KeK",
+
+            permissions: "*"
+
+        }]
+
+    }
+
 };
